d132d61c9bafaa817f897e6195c682bdf0ec2e1c
Andre-Philippe Paquet <appaquet@gmail.com>
2018-07-25 21:23:58 -0400

    Fix generator

--- build.rs
+++ build.rs
@@ -7,9 +7,12 @@ fn main() {
 
     if env::var("GENERATE_BINDINGS").is_ok() {
         let bindings = bindgen::Builder::default()
+            .clang_arg("-target")
+            .clang_arg("armv7-linux-androideabi") // see https://github.com/rust-lang-nursery/rust-bindgen/issues/1229
             .header("./headers/wrapper.h")
             .clang_args(&["-I./headers/"])
             .trust_clang_mangling(false) // otherwise it generates link_name with underscore as prefix and won't find the methods at runtime
+            .layout_tests(false) // not useful as we can't execute them on host
             .generate()
             .expect("Unable to generate bindings");
 
